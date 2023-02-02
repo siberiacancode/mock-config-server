332bfb752c8e0ec7044e3953324b43589a6fdfd1
Zianurov Nursultan <zianurovnursultan1@gmail.com>
2023-02-02 23:53:31 +0700

    #2 👹 fix bin/validate tests

--- bin/validateMockServerConfig/validateStaticPath/validateStaticPath.test.ts
+++ bin/validateMockServerConfig/validateStaticPath/validateStaticPath.test.ts
@@ -11,7 +11,7 @@ describe('validateStaticPath', () => {
       new Error(
         createValidationErrorMessage(
           'staticPath',
-          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[]'
+          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[] | undefined'
         )
       )
     );
@@ -19,7 +19,7 @@ describe('validateStaticPath', () => {
       new Error(
         createValidationErrorMessage(
           'staticPath',
-          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[]'
+          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[] | undefined'
         )
       )
     );
@@ -27,7 +27,7 @@ describe('validateStaticPath', () => {
       new Error(
         createValidationErrorMessage(
           'staticPath',
-          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[]'
+          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[] | undefined'
         )
       )
     );
@@ -66,7 +66,7 @@ describe('validateStaticPath', () => {
       new Error(
         createValidationErrorMessage(
           'staticPath',
-          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[]'
+          'string | { prefix: string; path: string } | (string | { prefix: string; path: string })[] | undefined'
         )
       )
     );
