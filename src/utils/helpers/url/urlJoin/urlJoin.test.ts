f5645b7a20d0180c5a4554ffb5223b6ee4a33c05
RiceWithMeat <47690223+RiceWithMeat@users.noreply.github.com>
2023-03-05 17:39:21 +0700

    #7 🐘 add leading slash in urlJoin result

--- src/utils/helpers/url/urlJoin/urlJoin.test.ts
+++ src/utils/helpers/url/urlJoin/urlJoin.test.ts
@@ -2,9 +2,13 @@ import { urlJoin } from './urlJoin';
 
 describe('urlJoin', () => {
   test('Should correctly merge paths', () => {
+    expect(urlJoin('/base', '/rest')).toEqual('/base/rest');
     expect(urlJoin('/base', 'rest')).toEqual('/base/rest');
-    expect(urlJoin('/base', 'rest')).toEqual('/base/rest');
-    expect(urlJoin('base', '/rest')).toEqual('base/rest');
-    expect(urlJoin('base', 'rest', '/users')).toEqual('base/rest/users');
+    expect(urlJoin('/base', 'rest', '/users')).toEqual('/base/rest/users');
+  });
+
+  test('Should add leading slash if it is not present in the result', () => {
+    expect(urlJoin('base', 'rest')).toEqual('/base/rest');
+    expect(urlJoin('base', '/rest')).toEqual('/base/rest');
   });
 });
--- src/utils/helpers/url/urlJoin/urlJoin.ts
+++ src/utils/helpers/url/urlJoin/urlJoin.ts
@@ -1,3 +1,6 @@
 import path from 'path';
 
-export const urlJoin = (...paths: string[]) => path.posix.join(...paths);
+export const urlJoin = (...paths: string[]) => {
+  const joinedPaths = path.posix.join(...paths);
+  return joinedPaths.startsWith('/') ? joinedPaths : `/${joinedPaths}`;
+};
