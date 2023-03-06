0fa6d568326872ba9389ebcc88666727d7fed800
RiceWithMeat <47690223+RiceWithMeat@users.noreply.github.com>
2023-03-06 18:03:11 +0700

    #7 🐘 use ternary operator in convertWin32PathToUnix

--- src/utils/helpers/url/urlJoin/urlJoin.ts
+++ src/utils/helpers/url/urlJoin/urlJoin.ts
@@ -4,11 +4,7 @@ import path from 'path';
 import { convertWin32PathToUnix } from '../convertWin32PathToUnix/convertWin32PathToUnix';
 
 export const urlJoin = (...paths: string[]) => {
-  let pathsToJoin = paths;
-
-  if (os.platform() === 'win32') {
-    pathsToJoin = paths.map((path) => convertWin32PathToUnix(path));
-  }
+  const pathsToJoin = os.platform() === 'win32' ? paths.map((path) => convertWin32PathToUnix(path)) : paths;
 
   return path.posix.join(...pathsToJoin);
 };
