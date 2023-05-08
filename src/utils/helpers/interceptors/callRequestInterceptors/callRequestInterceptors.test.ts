665371ac4fb7ec07a9eb3621fb9cc638dae2a5ee
debabin <dmitriybabinup@gmail.com>
2023-05-08 14:57:50 +0700

    ☄️ switch constants for test

--- src/utils/helpers/interceptors/callRequestInterceptors/callRequestInterceptors.test.ts
+++ src/utils/helpers/interceptors/callRequestInterceptors/callRequestInterceptors.test.ts
@@ -17,8 +17,8 @@ describe('callRequestInterceptors', () => {
     callRequestInterceptors({
       request,
       interceptors: {
-        apiInterceptor,
         requestInterceptor,
+        apiInterceptor,
         serverInterceptor
       }
     });
