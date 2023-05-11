ddb0eb1cc4c2ccced4fbd8142b75acd0472d4866
debabin <dmitriybabinup@gmail.com>
2023-05-11 23:26:44 +0700

    #49 ☄️ change it -> test

--- src/utils/helpers/parseCookie/parseCookie.test.ts
+++ src/utils/helpers/parseCookie/parseCookie.test.ts
@@ -1,42 +1,42 @@
 import { parseCookie } from './parseCookie';
 
 describe('parseCookie', () => {
-  it('Should return an empty object for empty string', () => {
+  test('Should return an empty object for empty string', () => {
     const cookieString = '';
     const cookies = parseCookie(cookieString);
 
     expect(cookies).toEqual({});
   });
 
-  it('Should return an object with a single key-value pair', () => {
+  test('Should return an object with a single key-value pair', () => {
     const cookieString = 'name=value';
     const cookies = parseCookie(cookieString);
 
     expect(cookies).toEqual({ name: 'value' });
   });
 
-  it('Should return an object with multiple key-value pairs for a cookie string with multiple key-value pairs', () => {
+  test('Should return an object with multiple key-value pairs for a cookie string with multiple key-value pairs', () => {
     const cookieString = 'name1=value1; name2=value2; name3=value3';
     const cookies = parseCookie(cookieString);
 
     expect(cookies).toEqual({ name1: 'value1', name2: 'value2', name3: 'value3' });
   });
 
-  it('Should handle cookies with no value by setting the value to an empty string', () => {
+  test('Should handle cookies with no value by setting the value to an empty string', () => {
     const cookieString = 'name1';
     const cookies = parseCookie(cookieString);
 
     expect(cookies).toEqual({ name1: '' });
   });
 
-  it('Should trim whitespace from keys and values', () => {
+  test('Should trim whitespace from keys and values', () => {
     const cookieString = ' name1 = value1 ; name2=value2;  name3   =   value3   ';
     const cookies = parseCookie(cookieString);
 
     expect(cookies).toEqual({ name1: 'value1', name2: 'value2', name3: 'value3' });
   });
 
-  it('Should ignore cookies with no name', () => {
+  test('Should ignore cookies with no name', () => {
     const cookieString = '=value';
     const cookies = parseCookie(cookieString);
 
