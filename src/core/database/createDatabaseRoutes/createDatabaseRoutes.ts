296d0ea6f5348bdc2bb61d0720901c687be06cd2
Nursultan Zianurov <zianurovnursultan1@gmail.com>
2023-08-10 21:58:52 +0700

    #53 👹 code style fixes

--- src/core/database/createDatabaseRoutes/createDatabaseRoutes.ts
+++ src/core/database/createDatabaseRoutes/createDatabaseRoutes.ts
@@ -12,24 +12,21 @@ import { FileStorage, MemoryStorage } from './storages';
 
 export const createDatabaseRoutes = (router: IRouter, { data, routes }: DatabaseConfig) => {
   if (routes) {
-    const storage =
-      typeof routes === 'string' ? new FileStorage(routes) : new MemoryStorage(routes);
+    const isRoutesJsonFile = typeof routes === 'string';
+    const storage = isRoutesJsonFile ? new FileStorage(routes) : new MemoryStorage(routes);
     createRewrittenDatabaseRoutes(router, storage.read());
 
-    // ✅ important:
-    // add endpoint for all routes
     router.route('/__routes').get((_request, response) => {
       response.json(storage.read());
     });
   }
 
-  const storage = typeof data === 'string' ? new FileStorage(data) : new MemoryStorage(data);
+  const isDataJsonFile = typeof data === 'string';
+  const storage = isDataJsonFile ? new FileStorage(data) : new MemoryStorage(data);
   const { shallowDatabase, nestedDatabase } = splitDatabaseByNesting(storage.read());
   createShallowDatabaseRoutes(router, shallowDatabase, storage as MemoryStorage<ShallowDatabase>);
   createNestedDatabaseRoutes(router, nestedDatabase, storage as MemoryStorage<NestedDatabase>);
 
-  // ✅ important:
-  // add endpoint for all database
   router.route('/__db').get((_request, response) => {
     response.json(storage.read());
   });
--- src/core/database/createDatabaseRoutes/helpers/createRewrittenDatabaseRoutes/createRewrittenDatabaseRoutes.ts
+++ src/core/database/createDatabaseRoutes/helpers/createRewrittenDatabaseRoutes/createRewrittenDatabaseRoutes.ts
@@ -4,8 +4,7 @@ import rewrite from 'express-urlrewrite';
 export const createRewrittenDatabaseRoutes = (
   router: IRouter,
   rewrittenRoutes: Record<string, string>
-) => {
+) =>
   Object.entries(rewrittenRoutes).forEach(([key, value]) => {
     router.use(rewrite(key, value));
   });
-};
