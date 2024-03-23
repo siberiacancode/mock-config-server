75a81ba44c5f5bc2c1b5db72d98c642cc825a5fe
Nursultan Zianurov <zianurovnursultan1@gmail.com>
2024-03-23 19:25:52 +0700

    #133 👹 add eslint config and tsconfig for bin/templates

---
+++ bin/templates/.eslintrc.js
@@ -0,0 +1,28 @@
+const { eslint } = require('@siberiacancode/eslint');
+
+module.exports = {
+  ...eslint.node,
+  overrides: [
+    ...eslint.node.overrides,
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: './bin/templates/tsconfig.json'
+      },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/await-thenable': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        'no-underscore-dangle': 'off',
+        'no-restricted-syntax': 'off',
+        'promise/always-return': ['error', { ignoreLastCallback: true }]
+      }
+    }
+  ]
+};
