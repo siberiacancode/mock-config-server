826d0fe55d127d91e326a56b35fb8edb01f263bd
babin <d.babin@cft.ru>
2024-11-25 19:42:09 +0700

    #192 🧊 delete useless files, add alias to swc

--- bin/helpers/index.ts
+++ bin/helpers/index.ts
@@ -1,6 +1,4 @@
 export * from './createTemplate';
-export * from './getMostSpecificPathFromError';
-export * from './getValidationMessageFromPath';
 export * from './isOnlyRequestedDataResolvingPropertyExists';
 export * from './resolveConfigFile';
 export * from './resolveConfigFilePath';
