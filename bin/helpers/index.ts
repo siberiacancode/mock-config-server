b0ec2629d1981cf208492bbb23c493324e0e2676
babin <d.babin@cft.ru>
2024-11-25 20:23:31 +0700

    #192 🧊 delete useless function

--- bin/helpers/index.ts
+++ bin/helpers/index.ts
@@ -1,5 +1,4 @@
 export * from './createTemplate';
-export * from './isOnlyRequestedDataResolvingPropertyExists';
 export * from './resolveConfigFile';
 export * from './resolveConfigFilePath';
 export * from './resolveExportsFromSourceCode';
